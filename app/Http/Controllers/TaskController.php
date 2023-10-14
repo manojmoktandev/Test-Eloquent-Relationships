087195721963c33bd01a1812c606e889230f8541
Manoj Moktan <developermanojmoktan@gmail.com>
2023-10-13 22:21:44 -0400

    Fix task1 test user create task

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::with('user')->paginate();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request)
+    {
+        // TASK: find out why this sentence fails, and fix it in Eloquent Model
+        auth()->user()->tasks()->create([
+            'name' => $request->name
+        ]);
+        return 'Success';
+    }
+}
